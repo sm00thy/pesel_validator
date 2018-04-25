4bdb4c0b0a545ce11f1b91c72d36842bb68c0a0f
sm00thy <ibalance5@gmail.com>
2018-04-25 20:23:22 +0200

    ver 1.0

--- Validator/Program.cs
+++ Validator/Program.cs
@@ -4,9 +4,46 @@ namespace Validator
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static class PeselValidator
+        { 
+            private static readonly int[] mnozniki = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+
+            public static bool ValidatePesel(string pesel)
+            {
+                bool toRet = false;
+                try
+                {
+                    if (pesel.Length == 11)
+                    {
+                        toRet = CountSum(pesel).Equals(pesel[10].ToString());
+                    }
+                }
+                catch (Exception)
+                {
+                    toRet = false;
+                }
+                return toRet;
+            }
+
+            private static string CountSum(string pesel)
+            {
+                int sum = 0;
+                for (int i = 0; i < mnozniki.Length; i++)
+                {
+                    sum += mnozniki[i] * int.Parse(pesel[i].ToString());
+                }
+
+                int reszta = sum % 10;
+                return reszta == 0 ? reszta.ToString() : (10 - reszta).ToString();
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Podaj numer PESEL: ");
+                string pesel = Console.ReadLine();
+
+                Console.WriteLine(ValidatePesel(pesel));
+            }
         }
     }
 }
